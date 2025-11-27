c804b7d085099fd93b096fc2d682b16619ece51a
felipebiancalana <felipe.biancalana@hotmail.com>
2025-11-27 11:27:30 -0300

    Create page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import "../../style.css";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Meu App Ativemind!</h1>
+      <p>Bem-vindo ao seu app simplificado para a Lasy.</p>
+    </div>
+  )
+}
